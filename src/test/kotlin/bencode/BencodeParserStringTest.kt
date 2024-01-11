c0855fb403b4a97ce094bf1f2d26d30a868dc3e6
tre3p <whereareyo@yahoo.com>
2024-01-11 17:39:54 +0300

    one more test case to string parser added

--- src/test/kotlin/bencode/BencodeParserStringTest.kt
+++ src/test/kotlin/bencode/BencodeParserStringTest.kt
@@ -8,19 +8,17 @@ class BencodeParserStringTest {
 
     @Test
     fun shouldCorrectlyParseBencodedString() {
-        val actual = parseBencode("5:hello").first
-        assertEquals("hello", actual)
-    }
+        val firstActual = parseBencode("5:hello")[0].first
+        val firstExpected = "hello"
+        assertEquals(firstExpected, firstActual)
 
-    @Test
-    fun shouldExtractExactCountOfCharsFromString() {
-        val actual = parseBencode("2:test").first
-        assertEquals("te", actual)
+        val secondActual = parseBencode("10:hellohello")[0].first
+        var secondExpected = "hellohello"
+        assertEquals(secondExpected, secondActual)
     }
 
     @Test
     fun shouldThrowExceptionOnInvalidStringLengthProvided() {
         assertThrows<BencodeParseException> { parseBencode("135:definitelynot135length") }
     }
-
 }
