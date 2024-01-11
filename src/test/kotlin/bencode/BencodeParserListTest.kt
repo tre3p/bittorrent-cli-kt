702af8167eac2f3173e7705e1d0a41ae65bacd06
tre3p <whereareyo@yahoo.com>
2024-01-11 19:44:28 +0300

    add bencoded lists parsing tests

---
+++ src/test/kotlin/bencode/BencodeParserListTest.kt
@@ -0,0 +1,26 @@
+package bencode
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class BencodeParserListTest {
+
+    @Test
+    fun shouldCorrectlyParseList() {
+        val bencodeString = "l11:bencodetesti42e4:hihie"
+        val expectedElements = listOf("bencodetest", 42, "hihi")
+
+        val actualList = parseBencode(bencodeString)[0].first
+        assertEquals(expectedElements, actualList)
+    }
+
+    @Test
+    fun shouldCorrectlyParseListWithNestedList() {
+        val bencodeString = "l5:helloli42e2:hiee"
+        val expectedElements = listOf("hello", listOf(42, "hi"))
+
+        val actualList = parseBencode(bencodeString)[0].first
+        assertEquals(expectedElements, actualList)
+    }
+
+}
